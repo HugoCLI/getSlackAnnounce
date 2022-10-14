d9e3d492b2e6f6f712ca237d5255f41c03f68786
root <root@vps-55a540cf.vps.ovh.net>
2022-10-14 13:35:46 +0000

    Optimize system

--- index.js
+++ index.js
@@ -1,73 +1,86 @@
-const express = require('express')
-const app = express();
-
-const c = require('colors');
-c.enable();
+// const express = require('express')
+// const app = express();
+const {Webhook} = require('discord-webhook-node');
+const request = require('request');
+const c = require('colors'); c.enable();
 const store = require('data-store')({path: process.cwd() + '/config.json'});
 
-const {Webhook} = require('discord-webhook-node');
-const hook = new Webhook(store.get('webhook-url'));
+const config = (type, value = null) => {
+    if (type && !value) {
+         return store.get(type);
+    }
+    else
+    {
+        return store.set(type, value);
+    }
+}
 
-const request = require('request');
+const Slack = {
+    "GetToken": "https://slack.com/api/oauth.v2.access",
+    "GetConversation": "https://slack.com/api/conversations.history",
+    "TargetChannelId": "C043TN0J3RD"
+}
 
-const users = store.get('users');
-let savedMessage = store.get('messages');
+
+const trace = (message) => console.log(c.yellow('[      axios-system ]') + " " + message);
+const hook = new Webhook(config('webhook-url'));
+const users = config('users');
+let savedMessage = config('messages');
 
 
-
-app.listen(2016, () => {
-    console.log('BotIsOnline')
+const onReadyStart = () => {
+    trace('bot is started');
 
     const CheckingAccessToken = () => {
-        if (!store.get('expires_in')) return console.error('user not authentified');
-        if (store.get('expires_in') > new Date().getTime()) return GettingChannelMessage();
+        if (!config('expires_in')) return console.error('user not authentified');
+        if (config('access_token') && config('expires_in') < new Date().getTime()) return GettingChannelMessage();
 
-        console.log('CheckingAccessToken');
-        const options = {
-            url: 'https://slack.com/api/oauth.v2.access?client_id=4071953744069.4235671570128&grant_type=refresh_token&client_secret='+store.get('client_secret') +'&refresh_token=' + store.get('refresh_token'),
-        };
+        let options = {};
+        options.url = `${Slack.GetToken}?client_id=${config('client_id')}&grant_type=refresh_token&client_secret=${config('client_secret')}&refresh_token=${config('refresh_token')}`;
+
         request(options, (error, response, body) => {
-            const args = JSON.parse(body);
-            store.set('access_token', args.access_token);
-            store.set('refresh_token', args.refresh_token);
-            store.set('expires_in', new Date().getTime() + (args.expires_in * 60000))
-            return GettingChannelMessage();
+              const args = JSON.parse(body);
+              config('access_token', args.access_token);
+              config('refresh_token', args.refresh_token);
+              config('expires_in', new Date().getTime() + (args.expires_in * 60000))
+              GettingChannelMessage();
         });
 
 
     }
 
     const GettingChannelMessage = () => {
-        if (!store.get('access_token')) return console.error('user not authentified');
+        if (!config('access_token')) return console.error('user not authentified');
 
-        console.log('GettingChannelMessage');
+        trace('GettingChannelMessage');
         const options = {
-            url: ' https://slack.com/api/conversations.history?channel=C043TN0J3RD',
+            url: `${Slack.GetConversation}?channel=${Slack.TargetChannelId}`,
             headers: {
-                "Authorization": `Bearer ${store.get('access_token')}`,
+                "Authorization": `Bearer ${config('access_token')}`,
             }
         };
         request(options, (error, response, body) => {
-            let data = JSON.parse(body);
-            for (const [key, value] of Object.entries(data))
+            const Result = JSON.parse(body);
+            for (const [key, value] of Object.entries(Result))
             {
                 for (let i = 0; value && i < value.length; i++)
                 {
-                    const element = value[i];
-                    if (element.type === "message" && !element.subtype)
+                    const Element = value[i];
+                    if (Element.type === "message" && !Element.subtype)
                     {
-                        const messageId = element.ts;
+                        const messageId = Element.ts;
+
                         if (!savedMessage.includes(messageId))
                         {
                             savedMessage.push(messageId);
-                            store.set('messages', savedMessage);
+                            config('messages', savedMessage);
 
                             // Running webhook
-                            const display_name = users[element.user] ? users[element.user] : element.user;
+                            const display_name = users[Element.user] ? users[Element.user] : Element.user;
 
                             hook.setUsername(`Slack`);
                             hook.setAvatar("https://a.slack-edge.com/80588/marketing/img/meta/slack_hash_256.png");
-                            hook.send(`**${display_name}** a écrit dans tls-annoucement:\n\n ${element.text}`);
+                            hook.send(`**${display_name}** a écrit dans tls-annoucement:\n\n ${Element.text}`);
 
                         }
                     }
@@ -77,15 +90,13 @@ app.listen(2016, () => {
         });
 
     }
-    CheckingAccessToken();
-
-
-
-
-
+  CheckingAccessToken();
+}
+onReadyStart();
 
 
     /*
+app.listen(2016, () => {
     Used only for robot initialization
 
     app.get('/api/slack', (req, res) => {
@@ -97,15 +108,12 @@ app.listen(2016, () => {
             url: 'https://slack.com/api/oauth.v2.access?client_id=4071953744069.4235671570128&client_secret=313be932cc128ec912478a2cc33fca39&code=' + code + '&redirect_uri=https://holberhelp.hugochilemme.com/api/slack',
         };
         request(options, (error, response, body) => {
-            const args = JSON.parse(body);
             store.set('access_token', args.authed_user.access_token);
             store.set('refresh_token', args.authed_user.refresh_token);
             store.set('expires_in', new Date().getTime() + (args.authed_user.expires_in * 60000));
             CheckingAccessToken();
         });
     })
-    */
+
 })
-
-
-const trace = (message) => console.log(c.yellow('[      axios-system ]') + " " + message);
+    */
